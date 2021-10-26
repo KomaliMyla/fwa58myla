d1e204ac254b7809e7c52c0b7e50ed23752e575b
KomaliMyla <89554583+KomaliMyla@users.noreply.github.com>
2021-10-26 00:21:33 -0500

    computation

---
+++ views/computation.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+var num1;
+
+
+
+/* GET users listing. */
+
+router.get("/", function (req, res, next) {
+  num1 = Math.random() * (100 - 1) + 1;
+  sin = Math.sin(num1);
+  abs = Math.ans(num1);
+  res.send(`log of ${num1} is: ${sin} <br>
+    
+         abs of ${-num1} is: ${abs}`);
+});
